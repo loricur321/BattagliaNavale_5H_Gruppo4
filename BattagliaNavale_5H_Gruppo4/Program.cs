4918205f5a8a70a848fe63d66f1010bc0594831d
loricur321 <loricur@gmail.com>
2022-04-05 09:44:19 +0200

    Start of play class and connectio  method

--- BattagliaNavale_5H_Gruppo4/Program.cs
+++ BattagliaNavale_5H_Gruppo4/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BattagliaNavale_5H_Gruppo4.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace BattagliaNavale_5H_Gruppo4
 {
@@ -10,7 +13,15 @@ namespace BattagliaNavale_5H_Gruppo4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I'm a server!");
+            WebSocketServer wbsv = new WebSocketServer("ws://127.0.0.1:9000");
+
+            wbsv.AddWebSocketService<PlayGame>("/PlayGame");
+
+            wbsv.Start();
+            Console.WriteLine("Server started on 127.0.0.1:9000");
+
+            Console.ReadKey();
+            wbsv.Stop();
         }
     }
 }
