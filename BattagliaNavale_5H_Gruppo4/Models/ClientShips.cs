f5ac3f4df6589ad0decc5cb62cf167d0ed65879a
loricur321 <loricur@gmail.com>
2022-04-22 11:51:30 +0200

    clientships

---
+++ BattagliaNavale_5H_Gruppo4/Models/ClientShips.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace BattagliaNavale_5H_Gruppo4.Models
+{
+    internal class ClientShips
+    {
+        public bool IsFull { get; set; }
+
+        public WebSocket Client { get; set; }
+
+        public Ship[] Ships { get; set; }
+
+        public ClientShips () { }
+
+        public ClientShips (bool isFull, WebSocket client, Ship[] ship)
+        {
+            this.IsFull = isFull;
+            this.Client = client;
+            this.Ships = ship;
+        }
+    }
+}
